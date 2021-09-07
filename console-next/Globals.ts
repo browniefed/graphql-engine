25660368a7999c11ec6ca1de83ba5ccbd31570cf
Jason Brown <browniefed@gmail.com>
2021-09-07 08:11:47 -0700

    Fix globals

--- console-next/Globals.ts
+++ console-next/Globals.ts
@@ -1,82 +1,119 @@
 /* eslint no-underscore-dangle: 0 */
-import { SERVER_CONSOLE_MODE } from "@/constants";
-import { getFeaturesCompatibility } from "@/helpers/versionUtils";
-import { stripTrailingSlash } from "@/components/Common/utils/urlUtils";
-import { isEmpty } from "@/components/Common/utils/jsUtils";
-import { Nullable } from "@/components/Common/utils/tsUtils";
+import { SERVER_CONSOLE_MODE } from "./constants";
+import { getFeaturesCompatibility } from "./helpers/versionUtils";
+import { stripTrailingSlash } from "./components/Common/utils/urlUtils";
+import { isEmpty } from "./components/Common/utils/jsUtils";
+import { Nullable } from "./components/Common/utils/tsUtils";
 
 type ConsoleType = "oss" | "cloud" | "pro" | "pro-cloud";
 
+declare global {
+  interface Window {
+    __env: {
+      nodeEnv: string;
+      apiHost: string;
+      apiPort: string;
+      dataApiUrl: string;
+      urlPrefix: string;
+      adminSecret: string;
+      isAdminSecretSet: boolean;
+      consoleMode: string;
+      enableTelemetry: boolean;
+      assetsPath: string;
+      serverVersion: string;
+      consolePath: string;
+      cliUUID: string;
+      consoleId: Nullable<string>;
+      herokuOAuthClientId: string;
+      tenantID: Nullable<string>;
+      projectID: Nullable<string>;
+      userRole: Nullable<string>;
+      cloudRootDomain: Nullable<string>;
+      luxDataHost: Nullable<string>;
+      consoleType: ConsoleType;
+      eeMode: Nullable<string>;
+    };
+  }
+  const CONSOLE_ASSET_VERSION: string;
+}
+
+const getWindow = () => (typeof window !== "undefined" ? window : undefined);
+
 /* initialize globals */
 
-const isProduction = process.env.NODE_ENV !== "development";
+const env = getWindow()?.__env;
+
+const isProduction = env?.nodeEnv !== "development";
+const CONSOLE_ASSET_VERSION = process.env.CONSOLE_ASSET_VERSION;
 
 const globals = {
-  apiHost: process.env?.apiHost,
-  apiPort: process.env?.apiPort,
-  dataApiUrl: stripTrailingSlash(process.env?.dataApiUrl), // overridden below if server mode
-  urlPrefix: stripTrailingSlash(process.env?.urlPrefix || "/"), // overridden below if server mode in production
-  adminSecret: process.env?.adminSecret || null, // gets updated after login/logout in server mode
+  apiHost: env?.apiHost,
+  apiPort: env?.apiPort,
+  dataApiUrl: stripTrailingSlash(env?.dataApiUrl), // overridden below if server mode
+  urlPrefix: stripTrailingSlash(env?.urlPrefix || "/"), // overridden below if server mode in production
+  adminSecret: env?.adminSecret || null, // gets updated after login/logout in server mode
   isAdminSecretSet:
-    process.env?.isAdminSecretSet ||
-    !isEmpty(process.env?.adminSecret) ||
-    false,
-  consoleMode: process.env?.consoleMode || process.env.SERVER_CONSOLE_MODE,
-  enableTelemetry: process.env?.enableTelemetry,
+    env?.isAdminSecretSet || !isEmpty(env?.adminSecret) || false,
+  consoleMode: env?.consoleMode || SERVER_CONSOLE_MODE,
+  enableTelemetry: env?.enableTelemetry,
   telemetryTopic: isProduction ? "console" : "console_test",
-  assetsPath: process.env?.assetsPath,
-  serverVersion: process.env?.serverVersion,
-  consoleAssetVersion: process.env.CONSOLE_ASSET_VERSION, // set during console build
-  featuresCompatibility: process.env?.serverVersion
-    ? getFeaturesCompatibility(process.env?.serverVersion)
+  assetsPath: env?.assetsPath,
+  serverVersion: env?.serverVersion,
+  consoleAssetVersion: CONSOLE_ASSET_VERSION, // set during console build
+  featuresCompatibility: env?.serverVersion
+    ? getFeaturesCompatibility(env?.serverVersion)
     : null,
-  cliUUID: process.env?.cliUUID,
+  cliUUID: env?.cliUUID,
   hasuraUUID: "",
   telemetryNotificationShown: false,
   isProduction,
-  herokuOAuthClientId: process.env?.herokuOAuthClientId,
-  hasuraCloudTenantId: process.env?.tenantID,
-  hasuraCloudProjectId: process.env?.projectID,
-  // cloudDataApiUrl: `${window.location?.protocol}//data.${process.env?.cloudRootDomain}`,
-  luxDataHost: process.env?.luxDataHost,
+  herokuOAuthClientId: env?.herokuOAuthClientId,
+  hasuraCloudTenantId: env?.tenantID,
+  hasuraCloudProjectId: env?.projectID,
+  cloudDataApiUrl: `${getWindow()?.location?.protocol}//data.${
+    env?.cloudRootDomain
+  }`,
+  luxDataHost: env?.luxDataHost,
   userRole: undefined, // userRole is not applicable for the OSS console
-  consoleType: process.env?.consoleType,
-  eeMode: process.env?.eeMode === "true",
+  consoleType: env?.consoleType,
+  eeMode: env?.eeMode === "true",
 };
-if (globals.consoleMode === process.env.SERVER_CONSOLE_MODE) {
-  if (!process.env?.dataApiUrl) {
-    // globals.dataApiUrl = stripTrailingSlash(window.location?.href);
+if (globals.consoleMode === SERVER_CONSOLE_MODE) {
+  if (!env?.dataApiUrl) {
+    globals.dataApiUrl = stripTrailingSlash(getWindow()?.location?.href);
   }
-  // if (isProduction) {
-  //   const consolePath = process.env?.consolePath;
-  //   if (consolePath) {
-  //     // let currentUrl = stripTrailingSlash(window.location?.href);
-  //     let slicePath = true;
-  //     if (process.env?.dataApiUrl) {
-  //       // currentUrl = stripTrailingSlash(process.env?.dataApiUrl);
-  //       slicePath = false;
-  //     }
-  //     const currentPath = stripTrailingSlash(window.location?.pathname);
+  if (isProduction) {
+    const consolePath = env?.consolePath;
+    if (consolePath) {
+      let currentUrl = stripTrailingSlash(getWindow()?.location?.href);
+      let slicePath = true;
+      if (env?.dataApiUrl) {
+        currentUrl = stripTrailingSlash(env?.dataApiUrl);
+        slicePath = false;
+      }
+      const currentPath = stripTrailingSlash(getWindow()?.location?.pathname);
 
-  //     // NOTE: perform the slice if not on team console
-  //     // as on team console, we're using the server
-  //     // endpoint directly to load the assets of the console
-  //     if (slicePath) {
-  //       globals.dataApiUrl = currentUrl.slice(
-  //         0,
-  //         currentUrl.lastIndexOf(consolePath)
-  //       );
-  //     }
+      // NOTE: perform the slice if not on team console
+      // as on team console, we're using the server
+      // endpoint directly to load the assets of the console
+      if (slicePath) {
+        globals.dataApiUrl = currentUrl.slice(
+          0,
+          currentUrl.lastIndexOf(consolePath)
+        );
+      }
 
-  //     globals.urlPrefix = `${currentPath.slice(
-  //       0,
-  //       currentPath.lastIndexOf(consolePath)
-  //     )}/console`;
-  //   } else {
-  //     // const windowHostUrl = `${window.location?.protocol}//${window.location?.host}`;
-  //     // globals.dataApiUrl = windowHostUrl;
-  //   }
-  // }
+      globals.urlPrefix = `${currentPath.slice(
+        0,
+        currentPath.lastIndexOf(consolePath)
+      )}/console`;
+    } else {
+      const windowHostUrl = `${getWindow()?.location?.protocol}//${
+        getWindow()?.location?.host
+      }`;
+      globals.dataApiUrl = windowHostUrl;
+    }
+  }
 }
 
 export default globals;
