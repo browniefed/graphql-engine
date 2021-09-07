a032d9d258f2df866835d42fbb0351f20fc4a67e
Jason Brown <browniefed@gmail.com>
2021-09-06 21:33:53 -0700

    React redux and some scss

--- console-next/pages/_app.tsx
+++ console-next/pages/_app.tsx
@@ -1,8 +1,15 @@
 import type { AppProps } from "next/app";
 import "../css/global.css";
 import "../css/reset.css";
+import "@/css/App.scss";
+import { Provider } from "react-redux";
+import { store } from "../store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store} key="provider">
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 export default MyApp;
